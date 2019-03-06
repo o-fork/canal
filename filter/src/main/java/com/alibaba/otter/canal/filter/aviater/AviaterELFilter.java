97983ac8d666728d2580c990839d2cc7b306de02
杨凯彬 <ykb553@163.com>
2019-03-06 19:41:30 +0800

    Aviater

--- filter/src/main/java/com/alibaba/otter/canal/filter/aviater/AviaterELFilter.java
+++ filter/src/main/java/com/alibaba/otter/canal/filter/aviater/AviaterELFilter.java
@@ -1,36 +1,37 @@
 package com.alibaba.otter.canal.filter.aviater;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.commons.lang.StringUtils;
-
 import com.alibaba.otter.canal.filter.CanalEventFilter;
 import com.alibaba.otter.canal.filter.exception.CanalFilterException;
 import com.alibaba.otter.canal.protocol.CanalEntry;
 import com.googlecode.aviator.AviatorEvaluator;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 基于aviater el表达式的匹配过滤
- * 
+ *
  * @author jianghang 2012-7-23 上午10:46:32
  */
 public class AviaterELFilter implements CanalEventFilter<CanalEntry.Entry> {
 
     public static final String ROOT_KEY = "entry";
-    private String             expression;
+    private String expression;
 
-    public AviaterELFilter(String expression){
+    public AviaterELFilter(String expression) {
         this.expression = expression;
     }
 
+    @Override
     public boolean filter(CanalEntry.Entry entry) throws CanalFilterException {
         if (StringUtils.isEmpty(expression)) {
             return true;
         }
 
-        Map<String, Object> env = new HashMap<String, Object>();
+        Map<String, Object> env = new HashMap<>();
         env.put(ROOT_KEY, entry);
+
         return (Boolean) AviatorEvaluator.execute(expression, env);
     }
 
--- filter/src/main/java/com/alibaba/otter/canal/filter/aviater/AviaterSimpleFilter.java
+++ filter/src/main/java/com/alibaba/otter/canal/filter/aviater/AviaterSimpleFilter.java
@@ -1,34 +1,29 @@
 package com.alibaba.otter.canal.filter.aviater;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.apache.commons.lang.StringUtils;
-
 import com.alibaba.otter.canal.filter.CanalEventFilter;
 import com.alibaba.otter.canal.filter.exception.CanalFilterException;
 import com.googlecode.aviator.AviatorEvaluator;
 import com.googlecode.aviator.Expression;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.*;
 
 /**
  * 基于aviater进行tableName简单过滤计算，不支持正则匹配
- * 
+ *
  * @author jianghang 2012-7-20 下午05:53:30
  */
 public class AviaterSimpleFilter implements CanalEventFilter<String> {
 
-    private static final String SPLIT             = ",";
+    private static final String SPLIT = ",";
 
     private static final String FILTER_EXPRESSION = "include(list,target)";
 
-    private final Expression    exp               = AviatorEvaluator.compile(FILTER_EXPRESSION, true);
+    private final Expression exp = AviatorEvaluator.compile(FILTER_EXPRESSION, true);
 
-    private final List<String>  list;
+    private final List<String> list;
 
-    public AviaterSimpleFilter(String filterExpression){
+    public AviaterSimpleFilter(String filterExpression) {
         if (StringUtils.isEmpty(filterExpression)) {
             list = new ArrayList<String>();
         } else {
@@ -37,6 +32,7 @@ public class AviaterSimpleFilter implements CanalEventFilter<String> {
         }
     }
 
+    @Override
     public boolean filter(String filtered) throws CanalFilterException {
         if (list.isEmpty()) {
             return true;
